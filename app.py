1c209ecdc3ce28ec34dfc4d46ea95f448dd747c8
SUNDAR_S <110710966+Sundar3274@users.noreply.github.com>
2025-02-07 22:21:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask on Render!"
+    return "Hello, Render!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Render assigns a dynamic port
+    app.run(host='0.0.0.0', port=port, debug=False)
